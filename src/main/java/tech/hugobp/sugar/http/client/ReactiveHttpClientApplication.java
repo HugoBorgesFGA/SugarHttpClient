192dc1c81c48962617e8e882a268a7918ee333ff
Hugo Pinto <hugobp14@hotmail.com>
2020-03-07 12:54:39 +0000

    Initial commit

---
+++ src/main/java/tech/hugobp/sugar/http/client/ReactiveHttpClientApplication.java
@@ -0,0 +1,13 @@
+package tech.hugobp.sugar.http.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveHttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveHttpClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/hugobp/sugar/http/client/ReactiveHttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hugobp.sugar.http.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveHttpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
